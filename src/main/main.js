4e15de7a6a35979b074fe52d05d02be838e6a5a8
Jesus24-Dev <siritjesus24@gmail.com>
2024-08-23 16:05:01 -0400

    Configuracion basica de la aplicacion

--- src/main/main.js
+++ src/main/main.js
@@ -3,7 +3,18 @@ const { app, BrowserWindow } = require("electron");
 let win;
 
 function createWindow() {
-  win = new BrowserWindow();
+  win = new BrowserWindow({
+    resizable: false,
+    title: "Flat World",
+    icon: "./src/assets/images/logo.png",
+    fullscreen: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: "./preload.js",
+    },
+  });
+  win.loadFile("./src/renderer/index.html");
 }
 
 app.whenReady().then(() => {
