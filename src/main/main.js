7f156adf48252b229cc524c0afed1c3422e0b5bc
Jesus24-Dev <siritjesus24@gmail.com>
2024-09-15 10:20:58 -0400

    Se agrego la logica de la pantalla de carga

--- src/main/main.js
+++ src/main/main.js
@@ -2,6 +2,27 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
 let win;
+let loadingWindow;
+
+function createLoadingWindow() {
+  loadingWindow = new BrowserWindow({
+    width: 450,
+    height: 450,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  loadingWindow.loadFile("./src/renderer/loading.html");
+
+  loadingWindow.on("closed", () => {
+    loadingWindow = null;
+  });
+}
 
 function createWindow() {
   win = new BrowserWindow({
@@ -9,6 +30,7 @@ function createWindow() {
     title: "Flat World",
     icon: "./src/assets/images/logo.png",
     fullscreen: true,
+    show: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -16,8 +38,26 @@ function createWindow() {
     },
   });
   win.loadFile("./src/renderer/index.html");
+  win.webContents.on("did-finish-load", () => {
+    setTimeout(() => {
+      loadingWindow.close();
+      win.show();
+    }, 1000);
+  });
+
+  win.on("closed", () => {
+    win = null;
+  });
 }
 
 app.whenReady().then(() => {
+  createLoadingWindow();
   createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
