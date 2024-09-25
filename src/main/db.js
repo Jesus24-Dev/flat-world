f760a9cf6a9abcfd333da56722302d9d81acecea
Jesus24-Dev <siritjesus24@gmail.com>
2024-09-25 17:51:02 -0400

    Revert "Se corrigio la ortografia"
    This reverts commit e5d0fb8693fc8b22ee05e3eccf4f6833dd01873a.

--- src/main/db.js
+++ src/main/db.js
@@ -8,9 +8,10 @@ class Perfil {
   }
 
   static async llenarDatos() {
-    const count = await Seccion.count();
+    const count = await Seccion.count(); // Cuenta los registros en la tabla
 
     if (count === 0) {
+      // Si no hay registros, poblar la tabla
       await Seccion.bulkCreate([
         { nombre: "A" },
         { nombre: "B" },
